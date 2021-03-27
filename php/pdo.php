92943899a0d5d7c548297ce7779b5e5c47d2e498
Harshil Padhiyar <66584783+harshilphs@users.noreply.github.com>
2021-03-27 15:12:57 +0530

    Update pdo.php

--- php/pdo.php
+++ php/pdo.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=sql302.epizy.com;dbname=epiz_28046518_personalSite','epiz_28046518','a8UTO20gASQzaG');
+$pdo = new PDO('mysql:host=local;dbname=xyz','username','pass');
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 ?>
